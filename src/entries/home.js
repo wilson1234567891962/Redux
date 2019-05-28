b679fb662e5263c1916a7fc5d0c25d28cecd02c9
Sem - 6 - INGENIERIA INDUSTRIAL: Wilson Alejandro Gonzalez Gaitan <wilsongonzalez.ag@academia.umb.edu.co>
2019-05-27 20:31:54 -0500

    Implements the providers

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,34 +1,35 @@
 import React from 'react';
-import { hydrate } from 'react-dom';
+import { render } from 'react-dom';
 import Home from '../pages/containers/home';
 // import Playlist from './src/playlist/components/playlist';
 import data from '../api.json';
 // console.log('Hola mundo!' )
 import { Provider } from 'react-redux';
+
 import { createStore } from 'redux';
-import { render } from 'react-dom';
+
+const initialState = {
+  data: {
+    ...data
+  }
+}
+
+const store = createStore(
+  (state) => state,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+console.log(store.getState());
 
 const homeContainer = document.getElementById('home-container')
 
-const initialState = {
-    data: {
-      ...data
-    }
-  }
-  
-  const store = createStore(
-    (state) => state,
-    initialState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
+// ReactDOM.render(que voy a renderizar, donde lo haré);
+// const holaMundo = <h1>hola Estudiante!</h1>;
 
-  console.log(store.getState());
-
-  
-  render(
-    <Provider store={store}>
-      <Home />
-    </Provider>
-  , homeContainer);
-  
+render(
+  <Provider store={store}>
+    <Home />
+  </Provider>
+, homeContainer);
 
